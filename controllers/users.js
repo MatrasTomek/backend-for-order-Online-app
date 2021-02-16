2ad8113a887c615105cd38c4715de00f6110bd58
DawidWebb <tomasz.matras@developerweb.pl>
2021-02-16 15:04:17 +0100

    feat: routes, contrillesr and models for user

---
+++ controllers/users.js
@@ -0,0 +1,104 @@
+// const { formsData } = require("./forms");
+const Login = require("../models/login");
+
+const usersData = [
+  {
+    accessLevel: 1,
+    // courses: [coursesData.map((course) => course.id)],
+    login: "",
+    password: "",
+  },
+];
+
+exports.postUser = (request, response, next) => {
+  try {
+    const { login, password } = request.body;
+
+    Login.find({}, (err, data) => {
+      usersData.login = data[0].login;
+      usersData.password = data[0].password;
+      console.log(usersData.login, usersData.password);
+    });
+
+    const user = usersData.find((u) => u.login === login);
+    if (!user) {
+      response.status(404).json({
+        message: "Użytkownik o podanym loginie nie istnieje",
+      });
+
+      return;
+    }
+
+    const isPasswordCorrect = user.password === password;
+    if (!isPasswordCorrect) {
+      response.status(401).json({
+        message: "Hasło lub login się nie zgadza",
+      });
+
+      return;
+    }
+
+    response.status(200).json({
+      user,
+    });
+  } catch (error) {
+    response.status(500).json({
+      error,
+      message:
+        "Oops! Coś poszło nie tak, przy metodzie POST w endpointcie /users",
+    });
+  }
+};
+
+// exports.patchUser = (request, response, next) => {
+//   try {
+//     const { login, courseId } = request.body;
+
+//     const course = coursesData.find((course) => course.id === courseId);
+//     const user = usersData.find((user) => user.login === login);
+
+//     if (!course) {
+//       response.status(404).json({
+//         message: "Nie znaleziono kursu o podanym Id",
+//       });
+
+//       return;
+//     } else if (!user) {
+//       response.status(404).json({
+//         message: "Nie znaleziono uzytkownika o podanym loginie",
+//       });
+
+//       return;
+//     }
+
+//     const hasUserCourseAlready = user.courses.some((id) => id === courseId);
+//     if (hasUserCourseAlready) {
+//       response.status(200).json({
+//         user,
+//       });
+
+//       return;
+//     }
+
+//     const hasUserEnoughtMoney = user.budget - course.price >= 0;
+//     if (!hasUserEnoughtMoney) {
+//       response.status(403).json({
+//         message: "Uzytkownik nie posiada wystarczających funduszy",
+//       });
+
+//       return;
+//     }
+
+//     user.budget = Number((user.budget - course.price).toFixed(2));
+//     user.courses.push(courseId);
+//     response.status(202).json({
+//       user,
+//     });
+//   } catch (error) {
+//     response.status(500).json({
+//       error,
+//       message:
+//         "Oops! Coś poszło nie tak, przy metodzie PATCH w endpointcie /users",
+//     });
+//   }
+// };
