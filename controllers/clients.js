de93251334a04749f34db0c0dc3f5246ef3a907d
DawidWebb <tomasz.matras@developerweb.pl>
2021-02-21 22:39:35 +0100

    feat: add function to get all clients from DB

--- controllers/clients.js
+++ controllers/clients.js
@@ -1,39 +1,15 @@
 const { v4: uuid } = require("uuid");
 const Client = require("../models/client.js");
 
-// new Admin({ login: "...", password: "... }).save();
-
-// const getData = (req, res, next) => {
-//   new Client({
-//     companyName: "test",
-//     companyAdress: "testowy 22, 00-00 Test",
-//     vatNo: "PL1231231223",
-//     eMail: "testowy@testowy.pl",
-//     info: "testowe info",
-//   }).save();
-
-//   Client.find({}, (err, data) => {
-//     usersData.login = data[0].login;
-//     usersData.password = data[0].password;
-//   });
-// };
-// getData();
-
-const clientsData = [
-  {
-    id: 000000,
-    companyName: "test",
-    companyAdress: "testowy 22, 00-00 Test",
-    vatNo: "PL1231231223",
-    eMail: "testowy@testowy.pl",
-    info: "testowe info",
-  },
-];
+// get all clients from DB
 
 exports.getClients = (request, response, next) => {
   try {
-    response.status(200).json({
-      clients: clientsData,
+    const findClients = Client.find();
+    findClients.exec((err, data) => {
+      response.status(200).json({
+        data,
+      });
     });
   } catch (error) {
     response.status(500).json({
@@ -44,31 +20,32 @@ exports.getClients = (request, response, next) => {
   }
 };
 
-exports.getClient = (request, response, next) => {
-  try {
-    const { vatNo } = request.params;
-    const clientToSend = clientsData.find((client) => client.vatNo === vatNo);
+// exports.getClient = (request, response, next) => {
+//   try {
+//     const { vatNo } = request.params;
+//     const clientToSend = clientsData.find((client) => client.vatNo === vatNo);
 
-    if (!clientToSend) {
-      response.status(404).json({
-        message: "Nie znaleziono klienta o podanym numerze nip",
-      });
+//     if (!clientToSend) {
+//       response.status(404).json({
+//         message: "Nie znaleziono klienta o podanym numerze nip",
+//       });
 
-      return;
-    }
+//       return;
+//     }
 
-    response.status(200).json({
-      client: clientToSend,
-    });
-  } catch (error) {
-    response.status(500).json({
-      error,
-      message:
-        "Oops! Coś poszło nie tak, przy metodzie GET w endpointcie /courses/:id",
-    });
-  }
-};
+//     response.status(200).json({
+//       client: clientToSend,
+//     });
+//   } catch (error) {
+//     response.status(500).json({
+//       error,
+//       message:
+//         "Oops! Coś poszło nie tak, przy metodzie GET w endpointcie /courses/:id",
+//     });
+//   }
+// };
 
+// add cleint to DB from addClientFrom
 exports.postClient = (request, response, next) => {
   const body = request.body;
   const newClient = new Client(body);
@@ -84,61 +61,62 @@ exports.postClient = (request, response, next) => {
   });
 };
 
-exports.putClient = (request, response, next) => {
-  try {
-    const { authors, id, price, title } = request.body;
-    if (!authors || !id || !price || !title) {
-      response.status(400).json({
-        message: "Nie podano wszystkich wymaganych informacji",
-      });
+// exports.putClient = (request, response, next) => {
+//   try {
+//     const { authors, id, price, title } = request.body;
+//     if (!authors || !id || !price || !title) {
+//       response.status(400).json({
+//         message: "Nie podano wszystkich wymaganych informacji",
+//       });
 
-      return;
-    }
+//       return;
+//     }
 
-    const indexCourseToUpdate = clientsData.findIndex(
-      (client) => client.vatNo === vatNo
-    );
-    if (indexClientToUpdate === -1) {
-      response.status(404).json({
-        message: "Nie znaleziono klienta o podanym numerze nip",
-      });
+//     const indexCourseToUpdate = clientsData.findIndex(
+//       (client) => client.vatNo === vatNo
+//     );
+//     if (indexClientToUpdate === -1) {
+//       response.status(404).json({
+//         message: "Nie znaleziono klienta o podanym numerze nip",
+//       });
 
-      return;
-    }
+//       return;
+//     }
 
-    clientsData.splice(indexCourseToUpdate, 1, request.body);
+//     clientsData.splice(indexCourseToUpdate, 1, request.body);
 
-    response.status(202).json({
-      clients: clientsData,
-    });
-  } catch (error) {
-    response.status(500).json({
-      error,
-      message:
-        "Oops! Coś poszło nie tak, przy metodzie PUT w endpointcie /clients",
-    });
-  }
-};
+//     response.status(202).json({
+//       clients: clientsData,
+//     });
+//   } catch (error) {
+//     response.status(500).json({
+//       error,
+//       message:
+//         "Oops! Coś poszło nie tak, przy metodzie PUT w endpointcie /clients",
+//     });
+//   }
+// };
 
 exports.deleteClient = (request, response, next) => {
   try {
-    const { vatNo } = request.params;
+    const findClients = Client.find();
+    findClients.exec((err, data) => {
+      const { vatNo } = request.params;
+      const indexClientToDelete = Client.findIndex(
+        (client) => client.vatNo === vatNo
+      );
 
-    console.log(vatNo);
-    const indexClientToDelete = clientsData.findIndex(
-      (client) => client.id === id
-    );
+      if (indexClientToDelete === -1) {
+        response.status(404).json({
+          message: "Nie znaleziono klienta o podanym numerze nip",
+        });
 
-    if (indexClientToDelete === -1) {
-      response.status(404).json({
-        message: "Nie znaleziono klienta o podanym numerze nip",
-      });
+        return;
+      }
 
-      return;
-    }
-
-    clientsData.splice(indexClientToDelete, 1);
-    response.status(200).end();
+      clientsData.splice(indexClientToDelete, 1);
+      response.status(200).end();
+    });
   } catch (error) {
     response.status(500).json({
       error,
@@ -148,4 +126,4 @@ exports.deleteClient = (request, response, next) => {
   }
 };
 
-exports.clientsData = clientsData;
+// exports.clientsData = clientsData;
--- routes/clients.js
+++ routes/clients.js
@@ -3,10 +3,10 @@ const clientsController = require("../controllers/clients");
 
 const router = express.Router();
 
-router.get("/:vatNo", clientsController.getClient);
 router.get("/", clientsController.getClients);
+// router.get("/:vatNo", clientsController.getClient);
 router.post("/", clientsController.postClient);
-router.put("/", clientsController.putClient);
+// router.put("/", clientsController.putClient);
 router.delete("/:vatNo", clientsController.deleteClient);
 router.use((request, response) => response.status(404).end());
 
