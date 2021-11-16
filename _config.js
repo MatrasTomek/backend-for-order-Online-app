a5309a08e71a60d321e39661fdcb37c9e24b3b46
DawidWebb <tomasz.matras@developerweb.pl>
2021-11-16 08:14:23 +0100

    chanage config

---
+++ _config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  db:
+    "mongodb+srv://admin:eppz6OwjRMmpiJVi@cluster0.18fea.mongodb.net/test?retryWrites=true&w=majority",
+  keySession: ["server-form-session"],
+  maxAgeSession: 1 * 60 * 60 * 1000, // 1h
+};
